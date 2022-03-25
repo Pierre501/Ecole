f0fab7fbfb4d16d3499e7ffd4434f4968dbe0e0e
Pierre Emmanuel <80822446+Pierre501@users.noreply.github.com>
2022-03-25 19:53:53 +0100

    Update Connexion.java

--- src/java/model/Connexion.java
+++ src/java/model/Connexion.java
@@ -12,7 +12,7 @@ public class Connexion
 	try
 	{
             Class.forName("org.postgresql.Driver");
-            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/gestion_de_ecole", "admin", "admin");
+            con = DriverManager.getConnection("jdbc:postgresql://ec2-3-225-213-67.compute-1.amazonaws.com:5432/d8euvg72fl0tjq", "liqwwjgrghnumg", "8305d4e4c51ca5e427fa097d060275bdfa877b8ebb45fd043429a3e679bc0272");
             con.setAutoCommit(false);
 	}
 	catch(Exception ex)
